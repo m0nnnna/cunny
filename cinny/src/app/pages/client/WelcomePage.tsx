9e39c74e76f9b9c02b6fee139224d683bc7029e2
Krishan <33421343+kfiven@users.noreply.github.com>
2025-09-29 14:34:38 +1000

    Release v4.10.1 (#2495)

--- cinny/src/app/pages/client/WelcomePage.tsx
+++ cinny/src/app/pages/client/WelcomePage.tsx
@@ -24,7 +24,7 @@ export function WelcomePage() {
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  v4.10.0
+                  v4.10.1
                 </a>
               </span>
             }
